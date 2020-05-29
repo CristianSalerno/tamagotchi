40cb25cd9674f34cfd1d9157dfc81f5ded0e24d8
CristianSalerno <cristiansalerno.18@gmail.com>
2020-05-29 14:47:50 +0200

    'assets'

--- src/components/TamagotchiBody.js
+++ src/components/TamagotchiBody.js
@@ -1,38 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 //Styles
-import '../styles/tamagotchi.scss'
+import "../styles/tamagotchi.scss";
 //Images
-import tamagotchi from '../assets/tamagotchi.jpeg'
-import tamagotchiPoo from '../assets/tamapoop.jpeg'
-import tamagotchiDirty from '../assets/tamaclean.jpeg';
-import tamagotchiFood from '../assets/tamafood.jpeg';
-import tamagotchiSad from '../assets/tamauwu.jpeg';
-
+import tamagotchi from "../assets/tamagotchi.jpeg";
+import tamagotchiPoo from "../assets/tamapoop.jpeg";
+import tamagotchiDirty from "../assets/tamaclean.jpeg";
+import tamagotchiFood from "../assets/tamafood.jpeg";
+import tamagotchiSad from "../assets/tamauwu.jpeg";
 
 function TamagotchiBody() {
-    const [creature, setCreature] = useState(tamagotchi)
+  const [creatureState, setCreatureState] = useState(tamagotchi);
 
-    const getCreature = () => {
-        setCreature(tamagotchiPoo)
+  const tamagotchiHandlerStates = () => {
+    setTimeout(() => {
+      setCreatureState(tamagotchiDirty);
+    }, 50000);
+  };
+
+  const getCreature = () => {
+    let creatureAsigned = tamagotchiPoo;
+
+    if (creatureState === creatureAsigned) {
+      setCreatureState(tamagotchi);
+    } else {
+      setCreatureState(creatureAsigned);
     }
+  };
 
-    return (
-        <div className="body">
-            <div className="screen">
-                <div className="screenLine--1" />
-                <div className="creature">
-                    <img src={creature} />
-                </div>
-                <div className="screenLine--2" />
-            </div>
-            <div className="buttons">
-                <div className="button button--a" onClick={getCreature}></div>
-                <div className="button button--b"></div>
-                <div className="button button--c"></div>
-            </div>
+  return (
+    <div className="body" state={tamagotchiHandlerStates()}>
+      <div className="screen">
+        <div className="screenLine--1" />
+        <div className="creature">
+          <img src={creatureState} />
         </div>
-    )
+        <div className="screenLine--2" />
+      </div>
+      <div className="buttons">
+        <div className="button button--a" onClick={getCreature}></div>
+        <div className="button button--b"></div>
+        <div className="button button--c"></div>
+      </div>
+    </div>
+  );
 }
 
 export default TamagotchiBody;
